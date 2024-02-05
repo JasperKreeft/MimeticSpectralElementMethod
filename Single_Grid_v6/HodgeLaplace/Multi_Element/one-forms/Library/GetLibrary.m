66479e78aacb53b6f5cc801a6cebdb43afbc776a
Jasper Kreeft <63003434+JasperShell@users.noreply.github.com>
2024-02-05 13:34:05 +0100

    sync MSEM_2

--- Single_Grid_v6/HodgeLaplace/Multi_Element/one-forms/Library/GetLibrary.m
+++ Single_Grid_v6/HodgeLaplace/Multi_Element/one-forms/Library/GetLibrary.m
@@ -9,7 +9,12 @@ switch in
             path(path,'C:\Users\Zolder\Documents\MATLAB\MSEM\Single_Grid_v6\HodgeLaplace\Multi_Element\Library_MultiElement')
             path(path,'C:\Users\Zolder\Documents\MATLAB\MSEM\Single_Grid_v6\HodgeLaplace\Multi_Element\one-forms\Library')
         elseif isunix
-
+            
+        elseif ismac
+            path(path,'/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/Library_SingleGrid')
+            path(path,'/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/HodgeLaplace/Library_HodgeLaplace')
+            path(path,'/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/HodgeLaplace/Multi_Element/Library_MultiElement')
+            path(path,'/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/HodgeLaplace/Multi_Element/one-forms/Library')
         end
 
     case 'finish'
@@ -20,7 +25,12 @@ switch in
             rmpath('C:\Users\Zolder\Documents\MATLAB\MSEM\Single_Grid_v6\HodgeLaplace\Multi_Element\Library_MultiElement')
             rmpath('C:\Users\Zolder\Documents\MATLAB\MSEM\Single_Grid_v6\HodgeLaplace\Multi_Element\one-forms\Library')
         elseif isunix
-
+            
+        elseif ismac
+            rmpath('/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/Library_SingleGrid')
+            rmpath('/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/HodgeLaplace/Library_HodgeLaplace')
+            rmpath('/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/HodgeLaplace/Multi_Element/Library_MultiElement')
+            rmpath('/Users/jasperkreeft/Documents/MATLAB/Codes/MSEM/Single_Grid_v6/HodgeLaplace/Multi_Element/one-forms/Library')
         end
 
 end
